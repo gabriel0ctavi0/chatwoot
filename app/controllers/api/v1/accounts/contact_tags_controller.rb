1969e29dd7c9504fae84d4ba14e4f41342c3923d
gabriel0ctavi0 <111649189+gabriel0ctavi0@users.noreply.github.com>
2026-02-04 19:04:11 -0300

    feat: Implement logging and error handling for contact tags operations

--- app/controllers/api/v1/accounts/contact_tags_controller.rb
+++ app/controllers/api/v1/accounts/contact_tags_controller.rb
@@ -5,14 +5,30 @@ class Api::V1::Accounts::ContactTagsController < Api::V1::Accounts::BaseControll
   before_action :fetch_contact_tag, except: [:index, :create]
   before_action :check_authorization
 
+  # #region agent log
+  around_action :log_contact_tags_errors
+  # #endregion
+
   def index
+    # #region agent log
+    debug_log_contact_tags('index_entry', action: 'index', account_user_set: Current.account_user.present?)
+    # #endregion
     @contact_tags = policy_scope(Current.account.contact_tags)
+    # #region agent log
+    debug_log_contact_tags('index_after_scope', action: 'index', count: @contact_tags.size)
+    # #endregion
   end
 
   def show; end
 
   def create
+    # #region agent log
+    debug_log_contact_tags('create_entry', action: 'create', params_keys: params.keys, has_contact_tag: params.key?(:contact_tag))
+    # #endregion
     @contact_tag = Current.account.contact_tags.create!(permitted_params)
+    # #region agent log
+    debug_log_contact_tags('create_success', action: 'create', id: @contact_tag.id)
+    # #endregion
   end
 
   def update
@@ -25,6 +41,43 @@ class Api::V1::Accounts::ContactTagsController < Api::V1::Accounts::BaseControll
   end
 
   private
+
+  # #region agent log
+  DEBUG_LOG_PATH = Rails.root.join('.cursor/debug.log').to_s.freeze
+
+  def log_contact_tags_errors
+    yield
+  rescue StandardError => e
+    File.open(DEBUG_LOG_PATH, 'a') do |f|
+      f.puts({
+        timestamp: Time.now.to_i,
+        location: 'contact_tags_controller',
+        message: 'contact_tags_error',
+        hypothesisId: 'H_error',
+        data: {
+          error_class: e.class.name,
+          error_message: e.message,
+          backtrace: e.backtrace&.first(10)
+        },
+        sessionId: 'debug-session'
+      }.to_json)
+    end
+    raise
+  end
+
+  def debug_log_contact_tags(msg, data = {})
+    File.open(DEBUG_LOG_PATH, 'a') do |f|
+      f.puts({
+        timestamp: Time.now.to_i,
+        location: 'contact_tags_controller',
+        message: msg,
+        hypothesisId: 'H_flow',
+        data: data,
+        sessionId: 'debug-session'
+      }.to_json)
+    end
+  end
+  # #endregion
 
   def fetch_contact_tag
     @contact_tag = Current.account.contact_tags.find(params[:id])
--- app/policies/contact_tag_policy.rb
+++ app/policies/contact_tag_policy.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 class ContactTagPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user_context, :user, :scope, :account, :account_user
+
+    def initialize(user_context, scope)
+      @user_context = user_context
+      @user = user_context[:user]
+      @account = user_context[:account]
+      @account_user = user_context[:account_user]
+      @scope = scope
+    end
+
+    def resolve
+      scope
+    end
+  end
+
   def index?
     @account_user.administrator? || @account_user.agent?
   end
