0215ec9c665bb525e63f1ad8847634971211f7b6
gabriel0ctavi0 <111649189+gabriel0ctavi0@users.noreply.github.com>
2026-02-12 10:30:01 -0300

    refactor: Simplify logo handling in Logo.vue by removing unused globalConfig and consolidating image source logic

--- app/javascript/dashboard/components-next/icon/Logo.vue
+++ app/javascript/dashboard/components-next/icon/Logo.vue
@@ -1,23 +1,14 @@
 <script setup>
 import { useAttrs } from 'vue';
-import { useMapGetter } from 'dashboard/composables/store';
 
 const attrs = useAttrs();
-const globalConfig = useMapGetter('globalConfig/get');
-const defaultLogoUrl = '/logo.svg';
+const sidebarLogoUrl = '/logo.svg';
 </script>
 
 <template>
   <img
-    v-if="globalConfig.logoThumbnail"
     v-bind="attrs"
-    :src="globalConfig.logoThumbnail"
-    class="rounded-lg"
-  />
-  <img
-    v-else
-    v-bind="attrs"
-    :src="defaultLogoUrl"
+    :src="sidebarLogoUrl"
     class="rounded-lg"
   />
 </template>
